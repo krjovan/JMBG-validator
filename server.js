1a7564541cf52b0873fe0d4a3550e9e38b6b5647
nteske <nemanjatesic96@gmail.com>
2020-11-29 00:59:10 +0100

    heroku fixes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = require('path');	const path = require('path');
+const path = require('path');
 
 const cors = require('cors');
-const app = express();	const app = express();
+const app = express();	
 
 
 app.use(cors());
 app.set('views', __dirname + '/dist/JMBG-validator');
 app.set('view engine', 'html');
 // Serve only the static files form the dist directory	// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/JMBG-validator'));	app.use(express.static(__dirname + '/dist/JMBG-validator'));
+app.use(express.static(__dirname + '/dist/JMBG-validator'));
