a1cb4bad0e74efe3bc21b71272b6b25aee0edf5d
Jovan Kron <jovankron97@hotmail.com>
2020-11-28 16:12:16 +0100

    Updated form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'JMBG-validator';
+
+  jmbg: string;
+  nizGresaka: string[];
+
+  ngOnInit(): void {
+    this.nizGresaka.push('prva greska');
+    this.nizGresaka.push('druga greska');
+    this.nizGresaka.push('treca greska');
+    this.nizGresaka.push('cetvrta greska');
+  }
+
+  validate(){
+    window.alert(this.jmbg);
+  }
 }
