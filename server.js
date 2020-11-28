ec09babe187eb04fbe394641ce7ade9335003c33
nteske <nemanjatesic96@gmail.com>
2020-11-29 00:42:54 +0100

    small mistake

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();	const app = express();
 
 
 app.use(cors());
-app.set('views', __dirname + '/dist/frontend');
+app.set('views', __dirname + '/dist/JMBG-validator');
 app.set('view engine', 'html');
 // Serve only the static files form the dist directory	// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontend'));	app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/JMBG-validator'));	app.use(express.static(__dirname + '/dist/JMBG-validator'));
